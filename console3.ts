bbf16750af43f633d863bd5ccb1e816fffe4787a
HoiSuen <johnnwong@hotmail.com>
2024-08-21 16:35:35 +0800

    css style files, and both check.png and myFavoriteicon.jpg are now able to be transferred now

--- console3.ts
+++ console3.ts
@@ -4,26 +4,53 @@ const port = 8080
 const { replaceStr } = require('./utility.js')
 const tooo = require('./tools');
 
-
+let count = 0;
 function serveStaticFile(res, path, contentType, responseCode) {
     if(!responseCode) responseCode = 200;
 
     // __dirname will resolve to the directory the executing script resides in.
     // So if your script resides in /home/sites/app.js, __dirname will resolve
     // to /home/sites.
+    count = count + 1;
+    console.log("count here, " + count + ":" + __dirname + path);
+    var fsTextfile = fs.createReadStream(__dirname + path);
+    if(contentType == 'text/html') {
+        
+        fsTextfile.on('open', function() {
+            console.log("html file here");
+            res.setHeader('Content-Type', contentType);
+            fsTextfile.pipe(res);
+            console.log("html file after piped!");
+        })
+    }
 
-    console.log(__dirname + path);
+    // change image/png to image/jpg
+    else if(contentType == 'image/jpg' || contentType == 'text/css' || contentType =='image/png') {
+        console.log("here is the contentType Var: " + contentType);
+        
+        fsTextfile.on('open', function() {
+            console.log("Before fileStream Piped!!!!!");
+            res.setHeader('Content-Type', contentType);
+            fsTextfile.pipe(res);
+            console.log("fileStream Piped!!!!!");
+        });
+        console.log("Has gone inside image_jpeg, image_png, text type");
+    }
 
+    /*
     fs.readFile(__dirname + path, function(err, data) {
         if(err) {
-            res.writeHead(500, { 'Content-Type' : 'text/plain' });
+            console.log("the file name: " + __dirname + path);
+            //res.writeHead(500, { 'Content-Type' : 'text/plain' });
             res.end('500 - Internal Error');
         } 
         else {
+            console.log("success here: " + __dirname + path);
             res.writeHead( responseCode, { 'Content-Type' : contentType });
             res.end(data);
         }
     });
+    */
 }
 
 
@@ -43,9 +70,18 @@ const server = http.createServer(function (req, res) {
         case '/check.png':
             serveStaticFile(res, '/check.png', 'image/png');
             break;
+        case '/myFavoriteicon.png':
+            serveStaticFile(res, '/myFavoriteicon.jpg', 'image/jpg');
+            break;
+        case '/myCustomStyle.css':
+            serveStaticFile(res, '/myCustomStyle.css', 'text/css');
+            break;
+        
+        /*
         default:
             serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
+        */
     }
 })
 
